ec4d75c0a1620d2704bb92a78e69780e3b902f80
MNiBorisov <MNiBorisov@get-lab.ru>
2024-12-10 15:16:55 +0300

    feat: коммит на тестирование 2

--- src/main/java/ru/get/Main.java
+++ src/main/java/ru/get/Main.java
@@ -2,8 +2,21 @@ package ru.get;
 
 public class Main {
     public static void main(String[] args) {
-        TaskExecutor taskExecutor = new TaskExecutor();
-//        taskExecutor.executeMyTask();
-        taskExecutor.executeMyTask2();
+        Conf conf = new Conf();
+        new Thread(() -> {
+            System.out.println("Поток 1 - " + conf.isTracingEnabled());
+            conf.enableTracing();
+            System.out.println("Поток 1 - " + conf.isTracingEnabled());
+        }).start();
+        new Thread(() -> {
+            System.out.println("Поток 2 - " + conf.isTracingEnabled());
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+//            conf.disableTracing();
+            System.out.println("Поток 2 - " + conf.isTracingEnabled());
+        }).start();
     }
 }
